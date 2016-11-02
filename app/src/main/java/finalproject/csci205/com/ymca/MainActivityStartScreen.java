cfc8366f5b3214358d9cae68ec9add212d721bf3
fr0hike <charles.hennessey@gmail.com>
2016-11-02 10:33:12 -0400

    Initial - Charles

---
+++ app/src/main/java/finalproject/csci205/com/ymca/MainActivityStartScreen.java
@@ -0,0 +1,13 @@
+package finalproject.csci205.com.ymca;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityStartScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_start_screen);
+    }
+}
