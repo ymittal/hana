07d7c19862919e43183133a1feb2b00e5e1aaa72
fr0hike <charles.hennessey@gmail.com>
2016-11-05 17:25:51 -0400

    Added Nav Drawer and a button to get to it

--- app/src/main/java/finalproject/csci205/com/ymca/MainActivityStartScreen.java
+++ app/src/main/java/finalproject/csci205/com/ymca/MainActivityStartScreen.java
@@ -1,16 +1,28 @@
 package finalproject.csci205.com.ymca;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivityStartScreen extends AppCompatActivity {
+
+    private Button tempLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_start_screen);
+        tempLogin = (Button) findViewById(R.id.tempLogin);
 
-        Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show();
+        tempLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Goto Other Activity
+                Intent i = new Intent(MainActivityStartScreen.this, NavActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
