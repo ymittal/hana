92168fff1b3f997a55ee14edcfe8d69cc33ca201
Malachi <malachimusick@yahoo.com>
2016-11-16 21:07:03 -0500

    WIP: random splash image

--- app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
+++ app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
@@ -1,11 +1,15 @@
 package finalproject.csci205.com.ymca.view;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
 
 import finalproject.csci205.com.ymca.R;
 import finalproject.csci205.com.ymca.view.util.SharedPreferenceUtil;
@@ -87,6 +91,17 @@ public class MainActivity extends AppCompatActivity {
      * @author Malachi Musick
      */
     private void loadBackground() {
+        //current implementation is static. TODO: Dynamically choose theBackground
 
+        RelativeLayout thisRL = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        Drawable theBackground;
+
+        //randomly pick a background
+        int r = new Random().nextInt(4);
+
+
+
+       // thisRL.setBackground(theBackground);
     }
 }
