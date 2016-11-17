4ed94ef8d8eac9831bc2496dfc326ca4878c7792
fr0hike <charles.hennessey@gmail.com>
2016-11-16 22:52:10 -0500

    Made the screen kinda pretty

--- countdown/src/main/java/finalproject/csci205/com/countcown/CountDownView.java
+++ countdown/src/main/java/finalproject/csci205/com/countcown/CountDownView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.CountDownTimer;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,11 +20,12 @@ import java.util.Date;
 public class CountDownView extends LinearLayout implements View.OnClickListener {
 
     private final int SECONDSPARAM = 1000;
+    ImageButton cancelPom;
+    private int startPauseCounter = 0;
+    private long storedTime;
     private TextView mins;
     private TextView seconds;
     private LinearLayout timerContainer;
-    private int startPauseCounter = 0;
-    private long storedTime;
     private CountDownTimer cdStart = null;
 
     public CountDownView(Context context) {
@@ -46,8 +48,11 @@ public class CountDownView extends LinearLayout implements View.OnClickListener 
         View root = inflate(getContext(), R.layout.countdownlayout, this);
         mins = (TextView) root.findViewById(R.id.mins);
         seconds = (TextView) root.findViewById(R.id.seconds);
+        cancelPom = (ImageButton) root.findViewById(R.id.cancelBtn);
+        cancelPom.setVisibility(GONE);
         timerContainer = (LinearLayout) root.findViewById(R.id.layoutContainer);
         timerContainer.setOnClickListener(this);
+        cancelPom.setOnClickListener(this);
     }
 
 
@@ -76,13 +81,22 @@ public class CountDownView extends LinearLayout implements View.OnClickListener 
 
 
         if (view.getId() == timerContainer.getId()) {
+            cancelPom.setVisibility(VISIBLE);
             startPauseCounter();
+        } else if (view.getId() == cancelPom.getId()) {
+            cdStart.cancel();
+            mins.setText("30"); //TODO Make same int from settings
+            seconds.setText("00");
+            storedTime = minToMili(30);
+            startPauseCounter++;
+            cancelPom.setVisibility(GONE);
         }
     }
 
     private void startPauseCounter() {
         if (startPauseCounter == 0) {
             startPauseCounter++;
+            //TODO get right param passing
             cdStart = new CountDownTimer(minToMili(30), SECONDSPARAM) {
                 @Override
                 public void onTick(long l) {
