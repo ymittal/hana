e09187ceb46326273b1139ca3b1edb7243f1f85a
Malachi <malachimusick@yahoo.com>
2016-11-16 21:41:19 -0500

    Added background randomization functionality

--- app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
+++ app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
@@ -1,7 +1,8 @@
 package finalproject.csci205.com.ymca.view;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
 
         loadBackground();
 
