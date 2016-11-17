b54fe1cf25feb6232a31d758ac1946f1d0a30aa6
Malachi <malachimusick@yahoo.com>
2016-11-16 20:48:35 -0500

    Added images

--- app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
+++ app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
@@ -1,9 +1,7 @@
 package finalproject.csci205.com.ymca.view;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -31,43 +29,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button tempLogin = (Button) findViewById(R.id.tempLogin);
 
-
-        //Check HERE if Shared Preference / Sugar data exists
-
-        if (!SharedPreferenceUtil.getIsOpen(getApplicationContext())) { // has not been opened before
-
-            Log.d("LOG_TAG", "false");
-
-
-            tempLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    SharedPreferenceUtil.setPreference(getApplicationContext());
-                    Intent i = new Intent(MainActivity.this, NavActivity.class);
-                    startActivity(i);
-                    finish();
-                    System.out.println("LOG_TAG " + SharedPreferenceUtil.getIsOpen(getApplicationContext()));
-                }
-            });
-
-        } else {                                            //If it has been opened before
+        if (SharedPreferenceUtil.getIsOpen(getApplicationContext())){
             Log.d("LOG_TAG", "true");
-            tempLogin.setVisibility(View.INVISIBLE);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    Intent i = new Intent(MainActivity.this, NavActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            }, 2000);
-
         }
+        else {
+            Log.d("LOG_TAG", "false");
+        }
+        /*
+            Check HERE if Shared Prefrence / Sugar data exists
 
-    }
-}
+            if (data isn't here) {
+
+            Button tempLogin = (Button) findViewById(R.id.tempLogin);
+            tempLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, NavActivity.class);
+                startActivity(i);
+                finish();
+            } else {
+
+                Intent i = new Intent(MainActivity.this, NavActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+            }
+         */
 
         //Note, to keep the close clean, refactor the else block into a method.
         //Here's some useful data to get you started
@@ -78,12 +69,8 @@ public class MainActivity extends AppCompatActivity {
             > https://developer.android.com/reference/android/content/SharedPreferences.html
          */
 
-<<<<<<< HEAD
 
         Button tempLogin = (Button) findViewById(R.id.tempLogin);
-=======
-/*
->>>>>>> 04565ffacca68058c9736bc2cfe45c43efb18818
         tempLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -103,4 +90,3 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
-*/
