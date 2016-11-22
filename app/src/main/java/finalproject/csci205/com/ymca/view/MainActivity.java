82ae88c23a756a974be654e4e31ccd81a61ce741
fr0hike <charles.hennessey@gmail.com>
2016-11-22 13:45:23 -0500

    Critical bug fix. Updated build tools so that this works on devices 4.4 and earlier.

--- app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
+++ app/src/main/java/finalproject/csci205/com/ymca/view/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //Makes the top status bar transparent. May change...
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
