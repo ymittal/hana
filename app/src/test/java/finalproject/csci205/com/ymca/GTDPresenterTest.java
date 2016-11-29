68473e8d33281a640328cbf6143dadc63819d46a
Aleksandar Antonov <ala021@bucknell.edu>
2016-11-29 05:59:59 -0500

    GTD Presenter testing. Utilizing blank constructor made for testing; Javadoc Considering mock library.

--- app/src/test/java/finalproject/csci205/com/ymca/GTDPresenterTest.java
+++ app/src/test/java/finalproject/csci205/com/ymca/GTDPresenterTest.java
@@ -1,8 +1,32 @@
 package finalproject.csci205.com.ymca;
 
-/**
- * Created by ceh024 on 11/13/16.
- */
+import org.junit.Test;
 
+import finalproject.csci205.com.ymca.model.Task;
+import finalproject.csci205.com.ymca.presenter.GTDPresenter;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *  Class for testing the functionality of various GTDPresenter
+ *  methods.
+ *
+ * Created by ala021 on 11/13/16.
+ */
 public class GTDPresenterTest {
+    /**
+     * Unit Test for adding tasks to the GTD Presenter
+     *
+     * @throws Exception
+     */
+    @Test
+    public void addTask() throws Exception {
+        GTDPresenter test = new GTDPresenter();
+        Task testTask = new Task();
+        test.addTask(testTask, true);
+        int expectedListLength = 1;
+        int actual = test.getTasks().size();
+        assertEquals(expectedListLength, actual);
+
+    }
 }
