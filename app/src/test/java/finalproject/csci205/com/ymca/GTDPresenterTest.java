791f09606fc7bfb2e5b45c14c2172ee21dcfaa79
Aleksandar Antonov <ala021@bucknell.edu>
2016-11-30 19:21:41 -0500

    Unit testing for GTD Presenter

--- app/src/test/java/finalproject/csci205/com/ymca/GTDPresenterTest.java
+++ app/src/test/java/finalproject/csci205/com/ymca/GTDPresenterTest.java
@@ -14,19 +14,36 @@ import static org.junit.Assert.assertEquals;
  * Created by ala021 on 11/13/16.
  */
 public class GTDPresenterTest {
+
+
     /**
      * Unit Test for adding tasks to the GTD Presenter
      *
      * @throws Exception
      */
     @Test
-    public void addTask() throws Exception {
+    public void test_AddTask() throws Exception {
         GTDPresenter test = new GTDPresenter();
         Task testTask = new Task();
         test.addTask(testTask, true);
         int expectedListLength = 1;
         int actual = test.getTasks().size();
         assertEquals(expectedListLength, actual);
+    }
 
+    /**
+     * Unit Test for adding tasks to the GTD Presenter
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test_removeTask() throws Exception {
+        GTDPresenter test = new GTDPresenter();
+        Task testTask = new Task();
+        test.addTask(testTask, true);
+        test.removeTask(0);
+        int expectedListLength = 0;
+        int actual = test.getTasks().size();
+        assertEquals(expectedListLength, actual);
     }
 }
