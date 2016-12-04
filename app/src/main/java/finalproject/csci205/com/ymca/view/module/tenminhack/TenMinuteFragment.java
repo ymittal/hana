caeee2519584a6946143449128764f888d15cdff
Malachi <malachimusick@yahoo.com>
2016-12-04 04:18:49 -0500

    Alarm in TenMinuteFragment now saves and updates on creation of the fragment as well.

--- app/src/main/java/finalproject/csci205/com/ymca/view/module/tenminhack/TenMinuteFragment.java
+++ app/src/main/java/finalproject/csci205/com/ymca/view/module/tenminhack/TenMinuteFragment.java
@@ -5,12 +5,13 @@ import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -22,13 +23,13 @@ import finalproject.csci205.com.ymca.R;
 
 public class TenMinuteFragment extends Fragment implements View.OnClickListener, TimePickerDialog.OnTimeSetListener {
 
+    protected static final String PREF_TENMIN_ALARM_MILLI_KEY = "PREF_TENMIN_ALARM_MILLI";
     private Calendar myCalendar;
-
     private TextView tvClock;
-
+    private Switch alarmSwitch;
     private AlarmManager alarmManager;
-
     private PendingIntent pendingIntent;
+    private SharedPreferences sharedPreferences;
 
 
     /**
@@ -41,6 +42,7 @@ public class TenMinuteFragment extends Fragment implements View.OnClickListener,
      * Use this factor method to create a new instance of {@link TenMinuteFragment}
      *
      * @return A new instance of {@link TenMinuteFragment}
+     * @author Yash
      */
     public static TenMinuteFragment newInstance() {
         TenMinuteFragment fragment = new TenMinuteFragment();
@@ -49,31 +51,54 @@ public class TenMinuteFragment extends Fragment implements View.OnClickListener,
         return fragment;
     }
 
-
+    /**
+     * A method called within the activity lifecycle. Comes before onCreate
+     *
+     * @param savedInstanceState
+     * @author Malachi
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myCalendar = Calendar.getInstance();
+        sharedPreferences = getActivity().getPreferences(Context.MODE_PRIVATE);
     }
 
+    /**
+     * The method called within the activity lifecycle. Comes after onCreate
+     * @param inflater
+     * @param container
+     * @param saveInstanceState
+     * @return
+     * @author Malachi
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle saveInstanceState) {
-
-        View root = inflater.inflate(R.layout.fragment_tenmin, container, false);
         getActivity().setTitle("10-Minute Hack");
 
+        View root = inflater.inflate(R.layout.fragment_tenmin, container, false);
         alarmManager = (AlarmManager) getContext().getSystemService(Context.ALARM_SERVICE);
-
         tvClock = (TextView) root.findViewById(R.id.tvClock);
+        alarmSwitch = (Switch) root.findViewById(R.id.alarmSwitch);
+
         tvClock.setOnClickListener(this);
 
-        //TODO: Get saved time from systemprefs
+        //if the user had set the alarm previously, update the view to this alarm's time
+        if (sharedPreferences.getLong(PREF_TENMIN_ALARM_MILLI_KEY, 0) != 0) {
+            myCalendar.setTimeInMillis(sharedPreferences.getLong(PREF_TENMIN_ALARM_MILLI_KEY, 0));
+        }
+        //Update the clock with whatever the calendar is set to
         updateTvClock(myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE));
 
         return root;
     }
 
+    /**
+     * When any view within this fragment is clicked, this method is called
+     * @param view The view within the fragment that was clicked
+     * @author Malachi
+     */
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.tvClock) {
@@ -85,6 +110,9 @@ public class TenMinuteFragment extends Fragment implements View.OnClickListener,
                     false).show();
         } else if (view.getId() == R.id.alarmSwitch) {
             //TODO: change SharedPreferences to alarm on or off
+            if (alarmSwitch.isChecked()) {
+                
+            }
         }
         Toast.makeText(getContext(), "Clicked", Toast.LENGTH_SHORT);
 
@@ -105,20 +133,25 @@ public class TenMinuteFragment extends Fragment implements View.OnClickListener,
      */
     @Override
     public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        //update the underlying calendar
         myCalendar.setTimeInMillis(System.currentTimeMillis());
         myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
         myCalendar.set(Calendar.MINUTE, minute);
 
+        //update the view
         updateTvClock(hourOfDay, minute);
 
+        //update the database
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putLong(PREF_TENMIN_ALARM_MILLI_KEY, myCalendar.getTimeInMillis());
+        editor.commit();
+
+        //Make the alarm
         Intent intent = new Intent(getContext(), AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(getContext(), 0, intent, 0);
-        //testing
-        // Log.i("AlarmSettings", "Current time_in_ms:     " + myCalendar.getTimeInMillis());
-        Log.i("AlarmSettings", "Alarm at time_in_ms:      " + myCalendar.getTimeInMillis());
         alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, myCalendar.getTimeInMillis(),
                 AlarmManager.INTERVAL_DAY, pendingIntent);
-        //  alarmManager.set(AlarmManager.RTC, myCalendar.getTimeInMillis(), pendingIntent);
 
     }
 
